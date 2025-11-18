e6c566aef9500dacd4b42fa0a3395e3be9cb8a10
Jackson Roodman <jacksonroodman@gmail.com>
2025-11-17 16:27:54 -0800

    Admin login with create post functionality. Created posts appear under blog tab.

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -10,6 +10,7 @@ import { getStorage } from 'firebase/storage'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+<<<<<<< HEAD
   apiKey: "AIzaSyBV4_lqQrP__2NsScNytKD764HjpqcFRIA",
   authDomain: "dawatucla.firebaseapp.com",
   projectId: "dawatucla",
@@ -17,6 +18,15 @@ const firebaseConfig = {
   messagingSenderId: "280953818987",
   appId: "1:280953818987:web:e9a07710423b109044c62e",
   measurementId: "G-1C8KYXMTHC"
+=======
+  apiKey: "AIzaSyCPSVT9h21rEClnRo1edxpwdtwAr1n9mZc",
+  authDomain: "blog-3f40e.firebaseapp.com",
+  projectId: "blog-3f40e",
+  storageBucket: "blog-3f40e.firebasestorage.app",
+  messagingSenderId: "523757664633",
+  appId: "1:523757664633:web:6779e9ed1e4a4e8ca1d7a8",
+  measurementId: "G-9BMGNLZX16"
+>>>>>>> c824231 (Admin login with create post functionality. Created posts appear under blog tab.)
 };
 
 // Initialize Firebase
