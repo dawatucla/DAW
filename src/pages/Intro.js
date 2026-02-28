1e1a5af25859c3cfafecdbbf36f5bf172b995bf2
Chris Sanrow <chris.sanrow@gmail.com>
2026-02-27 17:48:11 -0800

    Blog frontend css fixes

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,29 +1,24 @@
 import { Link } from "react-router-dom";
+import { Navbar } from "../components/Navbar"
 
 function Intro() {
   return (
     <div>
-            <div class="navbar">
-        <a href="../home/">HOME</a>
-        <a href="../connect/">CONNECT</a>
-        <a href="../events/">EVENTS</a>
-        <a href="../gallery/">GALLERY</a>
-        <a href="../blog/">BLOG</a>
-    </div>
-    <div className="sub-body">
-      <div className="logo-container">
-        <Link to="/home">
-          <img
-            src="/assets/dawlogotransparent.png"
-            alt="DAW Logo"
-          />
-        </Link>
+      <Navbar />
+      <div className="sub-body">
+        <div className="logo-container">
+          <Link to="/home">
+            <img
+              src="/assets/dawlogotransparent.png"
+              alt="DAW Logo"
+            />
+          </Link>
+        </div>
+              <footer>
+          <p>© DAW 2025</p>
+        </footer>
       </div>
-            <footer>
-        <p>© DAW 2025</p>
-      </footer>
-    </div>
-    </div>
+      </div>
   );
 }
 
